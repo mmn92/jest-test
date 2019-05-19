117c42f34d65fe190a795f65cda1edc42245d0fd
mmn92 <matheus.mota.nogueira@gmail.com>
2019-05-19 18:36:36 -0300

    test different types of matchers

--- functions.js
+++ functions.js
@@ -1,6 +1,13 @@
 const functions = {
   add: (num1, num2) => num1 + num2,
-  offsetAdd: (num1, num2) => num1 + num2 + 1
+  offsetAdd: (num1, num2) => num1 + num2 + 1,
+  isNull: () => null,
+  returnSame: a => a,
+  returnUser: () => ({
+    firstName: 'Mister',
+    lastName: 'Brother'
+  }),
+  square: x => x * x
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -14,6 +14,39 @@ test('Adds 2 + 2 to equal 4', () => {
 });
 
 // Failed test. Also using the .not modifier
+/*
+Leaving this test comment for now
 test('Adds 2 + 2 to not equal 5', () => {
   expect(functions.offsetAdd(2, 2)).not.toBe(5);
+});
+*/
+
+// Checks for null values
+test('Returns null', () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+// Checks for falsy values
+test('Returns falsy value', () => {
+  expect(functions.returnSame(0)).toBeFalsy();
+});
+
+/* 
+  Checks for the names in an user
+  In this instance, you must use the .toEqual matcher
+  If you use .toBe, the test will fail
+*/
+test('User should be Mister Brother', () => {
+  expect(functions.returnUser()).toEqual({
+    firstName: 'Mister',
+    lastName: 'Brother'
+  });
+});
+
+/* 
+  Testing for inequalities
+  You can check for values less than (or equal) and greater than (or equal)
+*/
+test('Should be less than, or equal 25', () => {
+  expect(functions.square(5)).toBeLessThanOrEqual(25);
 });
